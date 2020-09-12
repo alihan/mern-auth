a2175aa303e08591bd24ffeb2477d64b0c079c9a
alihany <alihanyiilmaz@gmail.com>
2020-09-12 22:30:42 +0300

    display new components on landing page

--- server/client/src/components/layout/Landing.js
+++ server/client/src/components/layout/Landing.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
 class Landing extends Component {
   render() {
     return (
-      <div className="container valign-wrapper">
+      <div style={{ height: '75vh' }} className="container valign-wrapper">
         <div className="row">
-          <div className="col s-12 center-align">
+          <div className="col s12 center-align">
             <h4>
               <b>Build</b> a login/auth app with the{' '}
-              <span stşye={{ fontFamily: 'monospace' }}>MERN</span> stack from
+              <span style={{ fontFamily: 'monospace' }}>MERN</span> stack from
               scratch
             </h4>
             <p className="flow-text grey-text text-darken-1">
@@ -38,7 +37,7 @@ class Landing extends Component {
                   borderRadius: '3px',
                   letterSpacing: '1.5px',
                 }}
-                className="btn btn-large btn-flat waves-effect white black-text"
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
                 Log In
               </Link>
@@ -49,5 +48,4 @@ class Landing extends Component {
     );
   }
 }
-
 export default Landing;
